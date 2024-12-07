720b2ddc72b1a22c6a9796f4447f0d9cebccf149
Marco Antonio <marcoantn020>
2024-12-07 12:55:55 -0300

    curriculum-2: curriculum-2: configure flowbite framework css

---
+++ app/Support/Functions.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+function user(): ?User
+{
+    if (Auth::check()) {
+        return Auth::user();
+    }
+
+    return null;
+}
